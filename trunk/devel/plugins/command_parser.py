64d8f2404fe2d189c8626afcdb802148c2bf385a
RoeBros <RoeBros@88673e9c-8449-0410-8e4d-315688a73b3d>
2008-04-12 19:52:34 +0000

    Fixed bug where bot could not send messages.

--- trunk/devel/plugins/command_parser.py
+++ trunk/devel/plugins/command_parser.py
@@ -5,16 +5,15 @@ class Plugin(base.Plugin):
         base.Plugin.__init__(self, bot, prefs)
 
     def priv_msg(self, user, channel, msg):
-        if msg[0] == "$":
-            msg = msg[1::]
-            cmd = msg.split()
-            if cmd[0] == "QUIT":
-##                self.bot.msg(self.bot.channel, user+": Quit, eh?")
+        cmd = msg.split()
+        if cmd[0] == "DO": #make sure this is a command not just a message!
+            cmd = cmd[1::]
+            if len(cmd) >= 1 and cmd[0] == "QUIT":
+                self.bot.msg(self.bot.channel, user+": Quit, eh?")
                 if self.bot.password and len(cmd) == 2 and\
                    cmd[1] == self.bot.password:
-##                    self.bot.msg(self.bot.channel, "I die!")
+                    self.bot.msg(self.bot.channel, "I die!")
                     self.bot.stop_serving()
                 else:
-##                    self.bot.msg(self.bot.channel, "Wrong password!")
-                    pass
+                    self.bot.msg(self.bot.channel, "Wrong password!")
                 
