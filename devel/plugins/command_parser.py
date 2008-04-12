f10ac9fee81664dfc52e252e32284e88fe533f9f
RoeBros <RoeBros@88673e9c-8449-0410-8e4d-315688a73b3d>
2008-04-12 03:56:50 +0000

    OK - modified the prefs file to handle lists - not nested! Also, plugins are now loaded based on what you say to load in the prefs.ini file. The msg system seems to be broken - the bot won't say anything!

--- devel/plugins/command_parser.py
+++ devel/plugins/command_parser.py
@@ -5,8 +5,16 @@ class Plugin(base.Plugin):
         base.Plugin.__init__(self, bot, prefs)
 
     def priv_msg(self, user, channel, msg):
-        if msg[0] == "!":
+        if msg[0] == "$":
             msg = msg[1::]
             cmd = msg.split()
-            if cmd == ["QUIT"]:
-                self.bot.stop_serving()
+            if cmd[0] == "QUIT":
+##                self.bot.msg(self.bot.channel, user+": Quit, eh?")
+                if self.bot.password and len(cmd) == 2 and\
+                   cmd[1] == self.bot.password:
+##                    self.bot.msg(self.bot.channel, "I die!")
+                    self.bot.stop_serving()
+                else:
+##                    self.bot.msg(self.bot.channel, "Wrong password!")
+                    pass
+                
